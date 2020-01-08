b32366e00e5f4a1fc054b9d9cf134154b6365816
nergel3 <alexnergel13@gmail.com>
2020-01-08 22:47:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Svelte-moving-div
+
+> A moving component for svelte js
+
+### Live demo
+
+> Coming soon !
+
+### Installation
+
+```
+npm install --save-dev svelte-mui
+```
+
+### Getting started
+
+```svelte
+<script>
+import MovingDiv from 'svelte-moving-div';
+</script>
+
+<div id="my-application">
+  <MovingDiv>
+    <h1>
+      Hello World!
+    </h1>
+  </MovingDiv>
+</div>
+
+<style>
+
+// The mouse must hover the moving-div
+:global(.moving-div) {
+  padding: 2rem;
+}
+
+// Adding transition to slow down
+:global(.moving-div) > :global(.inner-div) {
+  transition: transform .4s cubic-bezier(.04, .48, .98, .8)
+}
+</style>
+```
+
+### :stars: Show Your Support
+Please give a :star: if this project helped you!
+
+#### :scroll: License
+MIT © Alexandre Negrel
+
---
+++ src/index.svelte
@@ -0,0 +1,68 @@
+<div
+  bind:this={movingDiv}
+  on:mouseenter={trackMouse}
+  on:mousemove={updatePosition}
+  on:mouseleave={resetPosition}
+  class="moving-div"
+>
+  <div bind:this={innerDiv} class="inner-div">
+    <slot />
+  </div>
+</div>
+
+<script>
+let mouse = {
+  x: 0,
+  y: 0
+};
+
+let movingDiv,
+  innerDiv
+
+// Track the mouse when it hover the moving div
+function trackMouse() {
+  movingDiv.addEventListener('mousemove', setMouse);
+}
+
+function setMouse(event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+}
+
+function updatePosition() {
+  if (isHover(movingDiv)) {
+    const { top, left } = movingDiv.getBoundingClientRect(),
+      centerX = left + movingDiv.offsetWidth / 2,
+      centerY = top + movingDiv.offsetHeight / 2,
+      elPosX = (mouse.x - centerX) / 2,
+      elPosY = (mouse.y - centerY) / 2;
+
+    innerDiv.style.transform = `translate3d(${elPosX}px, ${elPosY}px, 0)`;
+    requestAnimationFrame(updatePosition);
+  } else {
+    resetPosition();
+  }
+}
+// Reset the moving div postion.
+function resetPosition() {
+  movingDiv.removeEventListener('mousemove', setMouse);
+  innerDiv.style.transform = 'translate3d(0, 0, 0)';
+}
+
+// Return if the given element is hovered
+function isHover(e) {
+  return e.parentElement.querySelector(':hover') === e;
+}
+
+</script>
+
+<style lang="sass">
+.moving-div
+  width: fit-content
+  cursor: pointer
+
+  & .inner-div
+    position: relative
+    pointer-events: none
+    transform: translate3d(0, 0, 0)
+</style>
